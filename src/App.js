cc1d45166c7f1d0118a6310f43da95df4cabe8e4
Oleg <olegc601@gmail.com>
2026-04-05 14:33:28 +0300

    1.3

--- src/App.js
+++ src/App.js
@@ -12,10 +12,18 @@ export default function App() {
       <Header>Hi</Header>
 
       <section>
-        
-        <Button text='Журнал отметок' isActive={tab === 'theLogOfMarks'} onClick={() => setTab('theLogOfMarks')}></Button>
-        <Button text='Отметить' isActive={tab === 'notice'} onClick={() => setTab('notice')}></Button>
-
+        <div style={{ 
+    display: 'grid', 
+    gridTemplateColumns: '1fr 1fr', 
+    gap: '10px', 
+    width: '100%' 
+}}>
+        <Button text='Журнал отметок' isActive={tab === 'theLogOfMarks'} onClick={() => setTab('theLogOfMarks')} style={{ width: '100%' }}></Button>
+        <Button text='Отметить' isActive={tab === 'notice'} onClick={() => setTab('notice')} style={{ width: '100%' }}></Button>
+        <div style={{ gridColumn: '1 / span 2'}}>
+        <Button text='Журнал лекарств' isActive={tab === 'listOfMedicines'} onClick={() => setTab('listOfMedicines')} style={{ width: '100%' }}></Button>
+        </div>
+        </div>
         {tab === 'notice' && <Notice />}
       </section>
     </div>
