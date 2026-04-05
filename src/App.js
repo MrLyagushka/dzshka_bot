8db8c6d89f38c61fc9f8824122228a1e49fb0f27
Oleg <olegc601@gmail.com>
2026-04-05 07:35:32 +0300

    1.1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import Header from './components/Header/Header';
+import Button from './components/Button/Button';
+import { useState } from 'react';
 
-function App() {
+export default function App() {
   return (
     <div>
-      Hi
+      <Header>Hi</Header>
+
+      <section>
+        <Button text='Журнал отметок'></Button>
+        <Button text='Отметить'></Button>
+      </section>
     </div>
   );
 }
-
-export default App;
