8e7c465217a6fce4e6b5c4a0046a659d991764bc
Oleg <olegc601@gmail.com>
2026-04-05 14:42:15 +0300

    1.3.2

--- src/App.js
+++ src/App.js
@@ -16,12 +16,12 @@ export default function App() {
     display: 'grid', 
     gridTemplateColumns: '1fr 1fr', 
     gap: '10px', 
-    width: '100%' 
+    width: '100%',
 }}>
-        <Button text='Журнал отметок' isActive={tab === 'theLogOfMarks'} onClick={() => setTab('theLogOfMarks')} style={{ width: '100%' }}></Button>
-        <Button text='Отметить' isActive={tab === 'notice'} onClick={() => setTab('notice')} style={{ width: '100%' }}></Button>
+        <Button text='Журнал отметок' isActive={tab === 'theLogOfMarks'} onClick={() => setTab('theLogOfMarks')} style={{ width: '100%', margin: 0 }}></Button>
+        <Button text='Отметить' isActive={tab === 'notice'} onClick={() => setTab('notice')} style={{ width: '100%', margin: 0 }}></Button>
         <div style={{ gridColumn: '1 / span 2'}}>
-        <Button text='Журнал лекарств' isActive={tab === 'listOfMedicines'} onClick={() => setTab('listOfMedicines')} style={{ width: '100%' }}></Button>
+        <Button text='Журнал лекарств' isActive={tab === 'listOfMedicines'} onClick={() => setTab('listOfMedicines')} style={{ width: '100%', margin: 0 }}></Button>
         </div>
         </div>
         {tab === 'notice' && <Notice />}
